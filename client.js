31c0a61e22b7fc7577a2da2f54a96befec2557bb
Emircan Çeitn <ctnemircan@gmail.com>
2021-12-29 19:37:16 +0300

    İnit

---
+++ client.js
@@ -0,0 +1,23 @@
+var soap = require('soap');
+var url = 'http://localhost:8000/wsdl?wsdl';
+
+// Create client
+soap.createClient(url, function (err, client) {
+  if (err){
+    throw err;
+  }
+  /* 
+  * Parameters of the service call: they need to be called as specified
+  * in the WSDL file
+  */
+  var args = {
+    plainText: "deneme"
+  };
+  // call the service
+  client.PasswordHasher(args, function (err, res) {
+    if (err)
+      throw err;
+      // print the service returned result
+    console.log(res);
+  });
+});
